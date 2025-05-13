3f07d86dc37a4407065e32917e6d1258a878d3b7
William <wcwcullwhitten@gmail.com>
2025-05-13 02:07:39 -0230

    hmm

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,7 +8,7 @@ export const Navbar = () => {
 
   return (
     <nav className={styles.navbar}>
-      <a className={`${styles.title} ${styles.fancyFont}`} href="/">
+      <a className={styles.title} href="/">
         William
       </a>
       <div className={styles.menu}>
